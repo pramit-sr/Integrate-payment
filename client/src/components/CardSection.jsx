ca92e00f190deae700cefa0a1210a9656d4583c2
pramit_sr <mepramitsarkarr@gmail.com>
2025-04-29 01:53:14 +0530

    No changes

--- client/src/components/CardSection.jsx
+++ client/src/components/CardSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios'; // 
+import axios from 'axios'; // 👈 make sure to import axios
 import Card from './Card';
 
 function CardSection() {
@@ -28,7 +28,7 @@ function CardSection() {
   const handleBuy = async (product) => {
     try {
       // 1. Create order on backend
-      const { data } = await axios.post('https://payment-lnvf.onrender.com/api/payment/createOrder', {
+      const { data } = await axios.post('http://localhost:4000/api/payment/createOrder', {
         amount: product.price * 100, // Razorpay expects paise
         currency: "INR",
         receipt: `receipt_order_${product.id}`,
@@ -48,7 +48,7 @@ function CardSection() {
           const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = response;
 
           try {
-            const verifyRes = await axios.post('https://payment-lnvf.onrender.com/api/payment/verifyPayment', {
+            const verifyRes = await axios.post('http://localhost:4000/api/payment/verifyPayment', {
               payment_id: razorpay_payment_id,
               order_id: razorpay_order_id,
               signature: razorpay_signature,
