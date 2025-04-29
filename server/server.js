9d5605359543e465a2a901e0c03e623349112619
pramit_sr <mepramitsarkarr@gmail.com>
2025-04-30 00:21:46 +0530

    small change for render

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ app.use(express.json());
 app.use('/api/payment', paymentRoutes);
 
 // Server start
+app.get('/', (req, res) => {
+  res.send('Backend is live');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
